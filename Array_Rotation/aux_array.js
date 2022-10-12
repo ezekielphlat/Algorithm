b6e732af727e41598058aeb3279a098d2cd52479
Ezekiel Abodesegun <eabodesegun@appzonegroup.com>
2022-10-12 18:30:25 +0100

    solve auxiliary array approach to this problem

---
+++ Array_Rotation/aux_array.js
@@ -0,0 +1,75 @@
+/*
+Using auxiliary array to implement array rotation
+
+Steps for left rotation
+last element must move to the left at r times
+Start
+Declear temp array of size r   (rotation count)
+Repeat this steps for r times (2)
+    temp[i]  = arr[i]  here we copy first two elements into the aux array
+Repeat this step for n - r times (5-2 = 3) repeat for 3 times  start i from r
+    arr[i] = arr[n-r+i]
+Repeat this steps for r times i will start from 0
+    (1+3) 4th
+    arr[i+n-r] = temp[i];
+
+Stop
+
+1st element must move to the right at r times
+Steps for Right rotation
+
+Start
+Declear temp array of size r   (rotation count)
+Repeat this steps for r times (2)
+    temp[i]  = arr[n-r +i]  we get the last two element
+Repeat this step for n - r times desc
+    arr[i] = arr[n-r -i]
+Repeat this steps for r times i will start from 0
+    (1+3) 4th
+    arr[i] = temp[i];
+
+Stop
+*/
+
+var arr = [1, 2, 3, 4, 5];
+
+var r = 2;
+const rotateLeft = (arr, r) => {
+  // declare the auxiliary array
+  let temp = [];
+  var n = arr.length;
+  for (let i = 0; i < r; i++) {
+    // copy the members of array to temp till r position
+    temp[i] = arr[i];
+  }
+  for (let i = r; i < n; i++) {
+    // shift the arrays from n-r position to the left
+    arr[i - r] = arr[i]; //
+  }
+  for (let i = 0; i < r; i++) {
+    arr[i + n - r] = temp[i];
+  }
+
+  return arr;
+};
+
+const rotateRight = (arr, r) => {
+  // declare the auxiliary array
+  let temp = [];
+  var n = arr.length;
+  for (let i = 0; i < r; i++) {
+    // copy the last members of the array
+    temp[i] = arr[n - r + i];
+  }
+  // console.log(temp);
+  for (let i = n - 1; i >= r; i--) {
+    // shift the element from the n-r position to the end of the array
+    arr[i] = arr[i - r]; //
+  }
+  for (let i = 0; i < r; i++) {
+    arr[i] = temp[i];
+  }
+
+  return arr;
+};
+console.log(rotateRight(arr, r));
--- Array_Rotation/bruteforce.js
+++ Array_Rotation/bruteforce.js
@@ -4,6 +4,8 @@ example
 [1,2,3,4,5] shifted to the right at a single rate is [5,1,2,3,4]
 The Bruteforce algorithm is below
 r is the rotation count
+
+Algotigthm for left rotation
 Start
 Declear Variable prev and temp
 Repeat the below step for r times
@@ -14,9 +16,21 @@ Repeat the below step for r times
         initialize prev = temp
 stop
 
+
+Algotigthm for right rotation
+Start
+Declear Variable prev and temp
+Repeat the below step for r times
+    initialize prev variable to arr[0]
+    Repeat this steps until j is -1
+        initialize temp  = arr[j]
+        initialize arr[j] = prev
+        initialize prev = temp
+stop
+
 */
 let arr = [1, 2, 3, 4, 5];
-const rotateArray = (arr, r) => {
+const rotateArrayRight = (arr, r) => {
   if (r == arr.length) {
     return arr;
   }
@@ -40,4 +54,29 @@ const rotateArray = (arr, r) => {
 };
 
 // console.log(rotateArray(arr, 2));
-console.log(rotateArray(arr, 5));
+// console.log(rotateArrayLeft(arr, 5));
+const rotateArrayLeft = (arr, r) => {
+  if (r == arr.length) {
+    return arr;
+  }
+  // declear temp and prev variable
+  let temp, prev;
+  let n = arr.length;
+  for (let i = 0; i < r; i++) {
+    // this is the last element in the array (5)
+    prev = arr[0];
+    // loop through each element in the array
+    for (let j = n - 1; j >= 0; j--) {
+      // keep the current element inside a temp variable (1)
+      temp = arr[j];
+      // move last element in current index  arr[j] = 5
+      arr[j] = prev;
+      // initialize prev value to temp (1)
+      prev = temp;
+    }
+  }
+  return arr;
+};
+
+//console.log(rotateArrayRight(arr, 2));
+console.log(rotateArrayLeft(arr, 2));
